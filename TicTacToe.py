7d4e393cb6881ba87b7d31566836a9518025b6a3
Ethan Barker <ethanbarker0@gmail.com>
2017-02-22 19:09:04 -0800

    Final

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,65 +1,68 @@
-#!/usr/bin/python
 # Imports
 from turtle import *
 from time import *
-from random import randint
-# Wait, left() exists? Oh...
+import random
+import os
+import subprocess
 
-hideturtle() # Hide turtle, cuz it's ugly
-tracer(0,0) # Used to remove turtle animation, update() to refresh screen
-setup(400,500) # Set window size # This comment is commenting about the comment about setting the window size in our code order to satisfy the requirment that requires comments on our code using comments.
-startInput = '' # Assume that the player wants to play if they launch the program
-cpuPlaying = False
+title("Tic Tac Toe")
+screen = getscreen()
+screen.listen()
+hideturtle() # Hide the turtle icon, cuz it's ugly
+tracer(0,0) # Used to remove turtle animation, update() to refresh screen now
+setup(400,500) # Set window size
+startInput = '' # Assume the player wants to play if they launch the program
+
+
 while startInput.lower() != "no" and startInput.lower() != "n": # Game loop
-    print("""
-    1 | 2 | 3
-    ---------
-    4 | 5 | 6
-    ---------
-    7 | 8 | 9
-    """) # Inform user of instructions
 
-    # Variables
-    grid = [0,0,0,0,0,0,0,0,0] # Set an array to store the grid inputs
-    winnerExists = False
-    playerUp = 1
+    # Display a quick intro, currently dissabled
+    def intro():
+        goto(0,0)
+        write("Tic", align="center", font=("Arial", 44, "bold"))
+        update()
+        sleep(1)
+        clear()
+        write("Tac", align="center", font=("Arial", 54, "bold"))
+        update()
+        sleep(1)
+        clear()
+        write("Toe", align="center", font=("Arial", 74, "bold"))
+        update()
+        sleep(1)
+        clear()
 
     # Functions to draw text
     def playUpDraw():
         penup()
-        right(270)
-        forward(50)
-        right(90)
-        write("Player up: " + str(playerUp), font=("Arial", 24, "bold"))
-        right(90)
-        forward(50)
-        right(270)
+        goto(0,200)
+        if cpuPlaying == True:
+            write("You are up", align="center", font=("Arial", 24, "bold"))
+            goto(-150, 150)
+            return
+        write("Player up: " + str(playerUp), align="center", font=("Arial", 24, "bold"))
+        goto(-150,150)
 
     def drawCat():
         penup()
-        right(270)
-        forward(50)
-        right(90)
-        write("Cat's Game", font=("Arial", 24, "bold"))
-        right(270)
-        forward(50)
-        right(90)
+        goto(0,200)
+        write("Cat's Game", align="center", font=("Arial", 24, "bold"))
+        goto(-150,150)
         update()
+    random.randint
 
     def playerWins(winner):
-        global winnerExists
         if winner != 0:
-            winnerExists = True
-            grid = ['']
             screenUpdate()
             penup()
-            right(270)
-            forward(50)
-            right(90)
-            write("Player " + str(winner) + " wins.", font=("Arial", 24, "bold"))
-            right(90)
-            forward(50)
-            right(270)
+            goto(0,200)
+            if cpuPlaying == True and winner == 1:
+                write("You lost to a computer", align="center", font=("Arial", 24, "bold"))
+                update()
+                goto(-150,150)
+                return
+            write("Player " + str(winner) + " wins.", align="center", font=("Arial", 24, "bold"))
+            goto(-150,150)
             update()
             print(str(winner) + " wins!")
 
@@ -111,36 +114,26 @@ while startInput.lower() != "no" and startInput.lower() != "n": # Game loop
     # Draw grid
     def drawGrid():
         penup()
-        forward(100)
+        goto(-50, 150)
         right(90)
         pendown()
-        forward(300)
+        goto(-50, -150)
         penup()
-        right(270)
-        forward(100)
-        right(270)
+        goto(50, 150)
         pendown()
-        forward(300)
+        goto(50, -150)
         penup()
-        right(90)
-        forward(100)
-        right(90)
-        forward(100)
+        goto(150, 50)
         right(90)
         pendown()
-        forward(300)
+        goto(-150, 50)
         penup()
-        right(270)
-        forward(100)
-        right(270)
+        goto(-150, -50)
         pendown()
-        forward(300)
+        backward(300)
         penup()
-        right(270)
-        forward(200)
-        right(270)
-        forward(300)
         right(180)
+        goto(-150,150)
 
     # Moves down a row and to the start
     def nextRow():
@@ -252,16 +245,26 @@ while startInput.lower() != "no" and startInput.lower() != "n": # Game loop
         canWin = cpuWinCheck()
         if canWin != False:
             grid[canWin] = 1
-        else:
-            canBlock = playerWinCheck()
-            if canBlock != False:
-                grid[canBlock] = 1
-            else:
-                while True:
-                    randomNum = randint(1,9)
-                    if checkForError(randomNum):
-                        grid[randomNum-1] = 1
-                        break
+            return
+        canBlock = playerWinCheck()
+        if canBlock != False:
+            grid[canBlock] = 1
+            return
+        if checkForError(5):
+            grid[4] = 1
+            return
+        toScramble = [0,2,6,8]
+        corners = scrambleArray(toScramble)
+        for i in toScramble:
+            if checkForError(i + 1):
+                grid[i] = 1
+                return
+        while True:
+            randomNum = random.randint(1,9)
+            if checkForError(randomNum):
+                print("random")
+                grid[randomNum-1] = 1
+                return
 
     def gridCopy(gridToCopy):
         copyGrid = []
@@ -269,6 +272,10 @@ while startInput.lower() != "no" and startInput.lower() != "n": # Game loop
             copyGrid.append(i)
         return copyGrid
 
+    def scrambleArray(arrayCopy):
+        scrambled = arrayCopy[:]
+        random.shuffle(scrambled)
+        return scrambled
 
     def cpuWinCheck():
         for i in range(0,9):
@@ -290,26 +297,54 @@ while startInput.lower() != "no" and startInput.lower() != "n": # Game loop
 
 
     def cat():
-        global winnerExists
         global grid
-        if 0 not in grid and winnerExists == False:
+        if 0 not in grid and detectWin(grid) == 0:
             print("Cat's game")
-            winnerExists == True
             drawCat()
-            grid = ['']
             return True
         return False
+
+    # Please ignore
+    def getH():
+        def getA():
+            def getC():
+                def getK():
+                    hack()
+                screen.onkey(getK, "k")
+            screen.onkey(getC, "c")
+        screen.onkey(getA, "a")
+
+    def getR():
+        def getE():
+            def getA():
+                def getK():
+                    breakGame()
+                screen.onkey(getK, "k")
+
+    # Please ignore
+    def hack():
+        subprocess.call("clear")
+        clear()
+        cwd =  os.path.dirname(os.path.realpath(__file__))
+        screen.bgpic(cwd + "/0.gif")
+        titles = ["HACKED", "hack3r", "HOW TO COMPUTER??!?!!", "WARNING VIRUS DETECTED", "hack.exe", "FATAL ERROR ENCOUNTERED", "OH NOSES", "please enter your SSN and credit info below", "NATE HACKING CORP"]
+        while True:
+            print('\a')
+            setup(random.randint(200, 400), random.randint(200,400))
+            print(titles[random.randint(0,8)])
+            title(titles[random.randint(0,8)])
+            sleep(0.01)
 
     def askInput():
         global playerUp # Make this variable global to apply across all functions
         if not (playerUp == 1 and cpuPlaying == True): # Check to see if CPU is playing
             playUpDraw() # Draw which player is up in text
             if playerUp == 1:
-                print("Player 1 Up. Choose number between 1 and 9")
+                print("Player 1 Up. Choose a number between 1 and 9")
             else:
-                print("Player 2 Up. Choose number between 1 and 9")
+                print("Player 2 Up. Choose a number between 1 and 9")
             checkError = True
-            while checkError:
+            while checkError: # This is our error checking, refer to each function for details
                 gridInput = input()
                 if checkForInt(gridInput):
                     gridInput = int(gridInput)
@@ -328,7 +363,6 @@ while startInput.lower() != "no" and startInput.lower() != "n": # Game loop
             CPU()
 
     def turn():
-        global winnerExists
         global playerUp
         if cat():
             return
@@ -337,15 +371,30 @@ while startInput.lower() != "no" and startInput.lower() != "n": # Game loop
             playerUp = 2
         else:
             playerUp = 1
+        print()
         winner = detectWin(grid)
         playerWins(winner)
-        if winnerExists == False:
+        if winner == 0:
             screenUpdate()
             turn()
         else:
             return
 
     # Start of Game
+    # intro()
+    screen.onkey(getH, "h")
+    screen.onkey(getR, "b")
+    print("""
+    1 | 2 | 3
+    ---------
+    4 | 5 | 6
+    ---------
+    7 | 8 | 9
+    """) # Inform user of instructions
+
+    # Variables
+    grid = [0,0,0,0,0,0,0,0,0] # Set an array to store the grid inputs
+    playerUp = 1
     penup()
     goto(-150,150) # Move start to the upper right
     pendown()
@@ -360,5 +409,7 @@ while startInput.lower() != "no" and startInput.lower() != "n": # Game loop
     turn() # Main Game
     # End of Game
     update()
-    restartInput=input ("Do you want to play again?") #This restarts the program.
+    print("Do you want to play again?")
+    restartInput=input() # This restarts the program.
     clear()
+# Wait, left() exists? Oh...
